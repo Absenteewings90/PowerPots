f5ae3b3390d4483830e8fb6d26d98596da280d30
David <54872406+Absenteewings90@users.noreply.github.com>
2026-03-31 16:49:54 -0400

    Add crafting recipes for PowerPots tiers 1-3

--- src/main/java/com/leo/powerpots/datagen/ModRecipeProvider.java
+++ src/main/java/com/leo/powerpots/datagen/ModRecipeProvider.java
@@ -13,6 +13,45 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+
+        // Tier 1
+        shapedRecipeBuilder.shaped(RecipeCategory.Misc, ModBlocks.POWER_BLOCKS.get(0).get())
+            .pattern("aaa")
+            .pattern("bcb")
+            .pattern("ded")
+            .define('a', Items.GRAY_CONCRETE)
+            .define('b', Items.RED_CONCRETE)
+            .define('c', Items.FLOWER_POT)
+            .define('d', Items.GREEN_CONCRETE)
+            .define('e', Items.REDSTONE_BLOCK)
+            .unlockedBy("has_flower_pot", has(Items.FLOWER_POT))
+            .save(pWriter);
+
+        // Tier 2
+        shapedRecipeBuilder.shaped(RecipeCategory.Misc, ModBlocks.POWER_BLOCKS.get(1).get())
+            .pattern("aaa")
+            .pattern("bcb")
+            .pattern("ded")
+            .define('a', Items.GRAY_CONCRETE)
+            .define('b', Items.RED_CONCRETE)
+            .define('c', ModBlocks.POWER_BLOCKS.get(0).get())
+            .define('d', Items.LAPIS_BLOCK)
+            .define('e', Items.REDSTONE_BLOCK)
+            .unlockedBy("has_power_pot_1", has(ModBlocks.POWER_BLOCKS.get(0).get()))
+            .save(pWriter);
+
+        // Tier 3
+        shapedRecipeBuilder.shaped(RecipeCategory.Misc, ModBlocks.POWER_BLOCKS.get(2).get())
+            .pattern("aaa")
+            .pattern("bcb")
+            .pattern("ded")
+            .define('a', Items.GRAY_CONCRETE)
+            .define('b', Items.RED_CONCRETE)
+            .define('c', ModBlocks.POWER_BLOCKS.get(1).get())
+            .define('d', Items.DIAMOND_BLOCK)
+            .define('e', Items.REDSTONE_BLOCK)
+            .unlockedBy("has_power_pot_2", has(ModBlocks.POWER_BLOCKS.get(1).get()))
+            .save(pWriter);
     }
 
 }
