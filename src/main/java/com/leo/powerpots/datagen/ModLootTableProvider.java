b9b2f4b41a0f0b593715a278ba80812258b99a95
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:17:36 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/datagen/ModLootTableProvider.java
+++ src/main/java/com/leo/powerpots/datagen/ModLootTableProvider.java
@@ -1,17 +1,19 @@
 package com.leo.powerpots.datagen;
 
 import com.leo.powerpots.datagen.loot.ModBlockLootTables;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
-import java.util.List;
-import java.util.Set;
-
 public class ModLootTableProvider {
-    public static LootTableProvider create(PackOutput output) {
-        return new LootTableProvider(output, Set.of(), List.of(
-            new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
-        ));
+    public ModLootTableProvider() {
+    }
+
+    public static LootTableProvider create(PackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
+        return new LootTableProvider(output, Set.of(), List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)), provider);
     }
 }
