8af12f4e05c01b7ee1cb8c93a3617ccc0028a6ee
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:09:24 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/init/ModCreativeTabs.java
+++ src/main/java/com/leo/powerpots/init/ModCreativeTabs.java
@@ -1,32 +1,37 @@
 package com.leo.powerpots.init;
 
-import com.leo.powerpots.PowerPots;
+import java.util.List;
+import java.util.Objects;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModCreativeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS;
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ITEMS;
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PowerPots.MODID);
+    public ModCreativeTabs() {
+    }
 
-    public static final RegistryObject<CreativeModeTab> ITEMS = CREATIVE_MODE_TABS.register("items", () ->
-        CreativeModeTab.builder()
-            .title(Component.translatable(PowerPots.MODID + ".itemGroup.items"))
-            .icon(Items.STICK::getDefaultInstance)
-            .displayItems((idp, output) -> {
-                Iterable<Item> items = ModItems.ITEMS.getEntries().stream().map(RegistryObject::get)::iterator;
-
+    static {
+        CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, "powerpots");
+        ITEMS = CREATIVE_MODE_TABS.register("items", () -> {
+            CreativeModeTab.Builder var10000 = CreativeModeTab.builder().title(Component.translatable("powerpots.itemGroup.items"));
+            Item var10001 = Items.STICK;
+            Objects.requireNonNull(var10001);
+            return var10000.icon(var10001::getDefaultInstance).displayItems((idp, output) -> {
+                List<? extends Item> items = Moditems.ITEMS.getEntries().stream().map(DeferredHolder::get).toList();
+                Objects.requireNonNull(output);
                 items.forEach(output::accept);
-
-                Iterable<Block> blocks = ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
-
+                List<? extends Block> blocks = ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).toList();
+                Objects.requireNonNull(output);
                 blocks.forEach(output::accept);
-            })
-            .build()
-    );
+            }).build();
+        });
+    }
 }
