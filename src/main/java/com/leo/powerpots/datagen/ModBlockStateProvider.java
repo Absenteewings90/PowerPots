463e29c1ae5d4b6dc97ad1343b34b6204f65a142
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:19:01 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/datagen/ModBlockStateProvider.java
+++ src/main/java/com/leo/powerpots/datagen/ModBlockStateProvider.java
@@ -1,34 +1,30 @@
 package com.leo.powerpots.datagen;
 
-import com.leo.powerpots.PowerPots;
-import com.leo.powerpots.config.Config;
 import com.leo.powerpots.init.ModBlocks;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ModelFile;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class ModBlockStateProvider extends BlockStateProvider {
-
-    ExistingFileHelper existingFileHelper;
+    private final ExistingFileHelper existingFileHelper;
 
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
-        super(output, PowerPots.MODID, exFileHelper);
+        super(output, "powerpots", exFileHelper);
         this.existingFileHelper = exFileHelper;
     }
 
-    @Override
     protected void registerStatesAndModels() {
-        for (RegistryObject<Block> block : ModBlocks.POWER_BLOCKS) {
-            simpleBlockWithItem(block);
+        for(DeferredHolder<Block, Block> block : ModBlocks.POWER_BLOCKS) {
+            this.simpleBlockWithItem(block);
         }
+
     }
 
-    private void simpleBlockWithItem(RegistryObject<Block> block) {
-        simpleBlockWithItem(block.get(), new ModelFile.UncheckedModelFile(new ResourceLocation(PowerPots.MODID, "block/" + block.getId().getPath())));
+    private void simpleBlockWithItem(DeferredHolder<Block, Block> block) {
+        this.simpleBlockWithItem((Block)block.get(), new ModelFile.UncheckedModelFile(ResourceLocation.fromNamespaceAndPath("powerpots", "block/" + block.getId().getPath())));
     }
 }
