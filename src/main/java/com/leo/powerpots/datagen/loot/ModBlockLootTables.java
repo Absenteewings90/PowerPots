883df182ecd839987c2e1dd5a449bd3c7f2c7351
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:20:51 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/datagen/loot/ModBlockLootTables.java
+++ src/main/java/com/leo/powerpots/datagen/loot/ModBlockLootTables.java
@@ -1,27 +1,45 @@
 package com.leo.powerpots.datagen.loot;
 
 import com.leo.powerpots.init.ModBlocks;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import mekanism.common.registration.impl.BlockRegistryObject;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.RegistryObject;
-
-import java.util.Set;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction;
+import net.minecraft.world.level.storage.loot.providers.nbt.ContextNbtProvider;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
-    public ModBlockLootTables() {
-        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    public ModBlockLootTables(HolderLookup.Provider provider) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), provider);
     }
 
     @Override
     protected void generate() {
-        ModBlocks.BLOCKS.getEntries().stream()
-            .map(RegistryObject::get)
-            .forEach(this::dropSelf);
+        for (Block block : ModBlocks.BLOCKS.getEntries().stream()
+                .map(DeferredHolder::value)
+                .toList()) {
+
+            this.add(block, LootTable.lootTable().withPool(
+                    LootPool.lootPool()
+                            .setRolls(ConstantValue.exactly(1))
+                            .add(LootItem.lootTableItem(block)
+                                    .apply(CopyComponentsFunction.copyComponents(CopyComponentsFunction.Source.BLOCK_ENTITY)
+                                    )
+                            )
+            ));
+        }
     }
 
-    @Override
     protected Iterable<Block> getKnownBlocks() {
-        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+        return (Iterable)ModBlocks.BLOCKS.getEntries().stream().map(DeferredHolder::get).collect(Collectors.toSet());
     }
 }
