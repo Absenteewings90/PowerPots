b6cbf186b877942c68a1e68efed894e71bd9d751
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:10:24 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/init/ModBlocks.java
+++ src/main/java/com/leo/powerpots/init/ModBlocks.java
@@ -1,55 +1,52 @@
 package com.leo.powerpots.init;
 
-import com.leo.powerpots.PowerPots;
 import com.leo.powerpots.block.PotTier;
-import com.leo.powerpots.block.PowerPotBlock;
 import com.leo.powerpots.config.Config;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
+import com.leo.powerpots.block.PowerPotBlock;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS;
+    public static List<DeferredHolder<Block, Block>> POWER_BLOCKS;
 
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, PowerPots.MODID);
-
-    public static List<RegistryObject<Block>> POWER_BLOCKS = new ArrayList<>();
+    public ModBlocks() {
+    }
 
     public static Block[] getBlocks() {
-        return POWER_BLOCKS.stream().map(RegistryObject::get).toArray(Block[]::new);
+        return (Block[])POWER_BLOCKS.stream().map(DeferredHolder::get).toArray((x$0) -> new Block[x$0]);
     }
 
     public static void register(IEventBus bus) {
-        for (PotTier tier : Config.INSTANCE.TIERS) {
-            if(tier.index() == 0) continue;
-            POWER_BLOCKS.add(
-                registerBlock("power_pot_" + tier.index(), () ->
-                    new PowerPotBlock(tier)
-                )
-            );
+        for(PotTier tier : Config.INSTANCE.TIERS) {
+            if (tier.index() != 0) {
+                POWER_BLOCKS.add(registerBlock("power_pot_" + tier.index(), () -> new PowerPotBlock(tier)));
+            }
         }
 
         BLOCKS.register(bus);
     }
 
-    public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+    public static <T extends Block> DeferredHolder<Block, T> registerBlock(String name, Supplier<T> block) {
+        DeferredHolder<Block, T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
         return toReturn;
     }
 
-    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    private static <T extends Block> void registerBlockItem(String name, DeferredHolder<Block, T> block) {
+        Moditems.ITEMS.register(name, () -> new BlockItem((Block)block.get(), new Item.Properties()));
+    }
+
+    static {
+        BLOCKS = DeferredRegister.create(BuiltInRegistries.BLOCK, "powerpots");
+        POWER_BLOCKS = new ArrayList<>();
     }
 }
