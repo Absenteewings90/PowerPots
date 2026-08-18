3aa5dd4feac546d6b1caa751355a94156a3f9669
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:18:16 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/leo/powerpots/datagen/ModBlockTagGenerator.java
@@ -1,39 +1,35 @@
 package com.leo.powerpots.datagen;
 
-import com.leo.powerpots.PowerPots;
-import com.leo.powerpots.config.Config;
 import com.leo.powerpots.init.ModBlocks;
+import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.data.BlockTagsProvider;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagGenerator extends BlockTagsProvider {
     public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
-        super(output, lookupProvider, PowerPots.MODID, existingFileHelper);
+        super(output, lookupProvider, "powerpots", existingFileHelper);
     }
 
-    @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        IntrinsicHolderTagsProvider.IntrinsicTagAppender<Block> tag = this.tag(BlockTags.MINEABLE_WITH_PICKAXE);
 
-        IntrinsicTagAppender<Block> tag = this.tag(BlockTags.MINEABLE_WITH_PICKAXE);
-
-        for (RegistryObject<Block> block : ModBlocks.POWER_BLOCKS) {
-            tag.add(block.get());
+        for(DeferredHolder<Block, Block> block : ModBlocks.POWER_BLOCKS) {
+            tag.add((Block)block.get());
         }
+
     }
 
     private static TagKey<Block> create(String pName) {
-        return TagKey.create(Registries.BLOCK, new ResourceLocation(PowerPots.MODID, pName));
+        return TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("powerpots", pName));
     }
 }
