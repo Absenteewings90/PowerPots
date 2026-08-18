633ff5d878dd83569df0f4b3ddcdea67bb0ac0a9
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:57:45 -0400

    version 1.1.0

--- src/main/java/com/leo/powerpots/event/ModBusClientEvent.java
+++ src/main/java/com/leo/powerpots/event/ModBusClientEvent.java
@@ -1,12 +1,23 @@
 package com.leo.powerpots.event;
 
 import com.leo.powerpots.PowerPots;
+import com.leo.powerpots.client.ModKeyBindings;
 import com.leo.powerpots.init.ModBlockEntities;
+import com.leo.powerpots.init.ModMenuTypes;
+import com.leo.powerpots.screen.UpgradeScreen;
 import net.darkhax.botanypots.block.BotanyPotRenderer;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import com.leo.powerpots.init.ModBlocks;
 
 @Mod.EventBusSubscriber(modid = PowerPots.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModBusClientEvent {
@@ -16,4 +27,18 @@ public class ModBusClientEvent {
         event.registerBlockEntityRenderer(ModBlockEntities.POWER_POT_BE.get(), BotanyPotRenderer::new);
     }
 
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            MenuScreens.register(ModMenuTypes.UPGRADE_MENU.get(), UpgradeScreen::new);
+            for (Block block : ModBlocks.getBlocks()) {
+                ItemBlockRenderTypes.setRenderLayer(block, RenderType.cutout());
+            }
+        });
+    }
+
+    @SubscribeEvent
+    public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
+        ModKeyBindings.onRegisterKeyMappings(event);
+    }
 }
