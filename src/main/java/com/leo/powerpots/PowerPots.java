6881c627669a4c72c97ddc3d179be96c1a5d4a6d
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:48:01 -0400

    Neoforge version 1.1.0

--- src/main/java/com/leo/powerpots/PowerPots.java
+++ src/main/java/com/leo/powerpots/PowerPots.java
@@ -1,41 +1,33 @@
 package com.leo.powerpots;
 
 import com.leo.powerpots.config.Config;
-import com.leo.powerpots.init.ModBlockEntities;
-import com.leo.powerpots.init.ModBlocks;
-import com.leo.powerpots.init.ModCreativeTabs;
-import com.leo.powerpots.init.ModItems;
+import com.leo.powerpots.init.*;
+import com.leo.powerpots.network.OpenUpgradeGuiPacket;
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(PowerPots.MODID)
+@Mod("powerpots")
 public class PowerPots {
-
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "powerpots";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-
-    public PowerPots() {
+    public PowerPots(IEventBus modEventBus, ModContainer modContainer) {
         Config.initialize();
 
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ModItems.ITEMS.register(modEventBus);
+        Moditems.ITEMS.register(modEventBus);
         ModUpgrades.SPEED_UPGRADE.getId();
         ModUpgrades.OUTPUT_UPGRADE.getId();
         ModUpgrades.ENERGY_UPGRADE.getId();
         ModUpgrades.FORTUNE_UPGRADE.getId();
 
-        ModBlocks.register(modEventBus);
         ModMenuTypes.MENUS.register(modEventBus);
-
+        ModBlocks.register(modEventBus);
         ModCreativeTabs.CREATIVE_MODE_TABS.register(modEventBus);
         ModBlockEntities.BLOCK_ENTITIES.register(modEventBus);
 
