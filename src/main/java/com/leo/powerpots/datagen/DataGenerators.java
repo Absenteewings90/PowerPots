ae766ac54355e86c665a20a411cc2452fee2af11
David <54872406+Absenteewings90@users.noreply.github.com>
2026-08-18 16:19:48 -0400

    NeoForge version 1.1.0

--- src/main/java/com/leo/powerpots/datagen/DataGenerators.java
+++ src/main/java/com/leo/powerpots/datagen/DataGenerators.java
@@ -1,18 +1,21 @@
 package com.leo.powerpots.datagen;
 
-import com.leo.powerpots.PowerPots;
+import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-import java.util.concurrent.CompletableFuture;
-
-@Mod.EventBusSubscriber(modid = PowerPots.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(
+        modid = "powerpots"
+)
 public class DataGenerators {
+    public DataGenerators() {
+    }
+
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
@@ -20,11 +23,12 @@ public class DataGenerators {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> provider = event.getLookupProvider();
 
+        // Client-side
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
-        generator.addProvider(event.includeClient(), new ModRecipeProvider(packOutput));
 
-        generator.addProvider(event.includeClient(), ModLootTableProvider.create(packOutput));
-
-        generator.addProvider(event.includeClient(), new ModBlockTagGenerator(packOutput, provider, existingFileHelper));
+        // Server-side
+        generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput, provider));
+        generator.addProvider(event.includeServer(), new ModBlockTagGenerator(packOutput, provider, existingFileHelper));
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, provider));
     }
 }
